782c2a5470c88bee520495abec813ac1ba32bd12
sathya-026 <sathyabayagani26@gmail.com>
2026-06-21 00:40:59 +0530

    test: debug logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Public } from "@common/decorators";
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Req } from "@nestjs/common";
+import { Request } from "express";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,16 @@ export class AppController {
     @Get('health')
     getHealth(): { status: string } {
         return { status: 'OK' };
+    }
+
+    @Get('debug-auth')
+    debug(@Req() req: Request) {
+        console.log(req.cookies);
+        console.log(req.headers.cookie);
+
+        return {
+            cookies: req.cookies,
+            cookieHeader: req.headers.cookie,
+        };
     }
 }
