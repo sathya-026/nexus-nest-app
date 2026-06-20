63ef0ade6cb304264ea3ac5f5112632c15e395c0
sathya-026 <sathyabayagani26@gmail.com>
2026-06-20 20:43:16 +0530

    update: public access to heath endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
+import { Public } from "@common/decorators";
 import { Controller, Get } from "@nestjs/common";
 
 @Controller()
 export class AppController {
+
+    @Public()
     @Get('health')
     getHealth(): { status: string } {
         return { status: 'OK' };
